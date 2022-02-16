0812b0f7f11c9403f5e8d322f2cc006a705e1a2f
isaacalderonURSA <isaac.calderon@ursa-systems.com.mx>
2022-02-16 11:48:54 -0800

    CORS: https://localhost:7006 http://localhost:5032

--- codingAssigment/Program.cs
+++ codingAssigment/Program.cs
@@ -1,7 +1,24 @@
 global using codingAssigment.Data;
 using Microsoft.EntityFrameworkCore;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy(name: MyAllowSpecificOrigins,
+		builder =>
+		{
+			//builder.WithOrigins("https://localhost:7006", "https://localhost:5032");
+			builder.SetIsOriginAllowed(origin =>
+				new Uri(origin).Host == "localhost" &&
+				(new Uri(origin).Port == 7006 || new Uri(origin).Port == 5032)
+			)
+			.AllowAnyHeader()
+			.AllowAnyMethod();
+		});
+});
 
 // Add services to the container.
 
@@ -27,6 +44,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
